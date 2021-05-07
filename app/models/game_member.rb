fb33e55b3bddf415254721920b49d05979f00a9b
rutani-thakkar <rutani.thakkar105@gmail.com>
2021-05-07 19:23:35 +0530

    Added validation for game members

--- app/models/game_member.rb
+++ app/models/game_member.rb
@@ -4,4 +4,9 @@ class GameMember < ApplicationRecord
 
   scope :win, -> { where(is_winner: true) }
   scope :lose, -> { where(is_winner: false) }
+  validate :check_members_length
+
+  def check_members_length
+    errors.add("Members can not be added more than 2") if self.game.game_members.count >= 2
+  end
 end
